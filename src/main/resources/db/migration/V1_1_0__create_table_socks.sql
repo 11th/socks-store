7786edbf38237ad22b603d73013c05de18e5868b
11th <11th@bk.ru>
2023-08-05 02:28:45 +0300

    change database

--- src/main/resources/db/migration/V1_1_0__create_table_socks.sql
+++ src/main/resources/db/migration/V1_1_0__create_table_socks.sql
@@ -1,10 +1,8 @@
 CREATE TABLE IF NOT EXISTS socks
 (
-    id          INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id          SERIAL      NOT NULL PRIMARY KEY,
     color       VARCHAR(30) NOT NULL,
     cotton_part INT         NOT NULL CHECK ( cotton_part >= 0 ),
-    quantity    INT         NOT NULL CHECK ( quantity >= 0 )
+    quantity    INT         NOT NULL CHECK ( quantity >= 0 ),
+    unique (color, cotton_part)
 );
-
-ALTER TABLE socks
-    ADD UNIQUE unique_socks (color, cotton_part);
